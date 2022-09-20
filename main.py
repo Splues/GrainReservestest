666a0c9e57572c4b9625c6e0dc73ef4caa3eff47
ssy <752611487@qq.com>
2022-09-20 22:47:07 +0800

    优化测试报告输出

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ if __name__ == '__main__':
     #suit.addTest(Testcase.TestCalculator("test_case_2"))
     suit.addTest(Testcase.TestCalculator("test_case_3"))
 
-    #存储html测试报告
-    f = open('./result.html', 'w', encoding='utf-8')
+
 
     #模式选择
     modul = 2
 
     #创建测试运行器
     if modul == 1:
+        # 存储html测试报告
+        f = open('./result.html', 'w', encoding='utf-8')
         runner = HTMLTestRunner.HTMLTestRunner(stream=f, title='百度搜索测试报告', description='用例执行情况：')
     elif modul == 2:
         runner = unittest.TextTestRunner()
@@ -34,6 +35,10 @@ if __name__ == '__main__':
     #参数1：存储报告地址；参数2：标题；参数3：小标题；
     runner.run(suit)
 
-    f.close()
+    if modul == 1:
+        f.close()
+    else:
+        print("未输出测试报告")
+
 
 
